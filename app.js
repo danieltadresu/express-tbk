ac563411c92b1fc7391651db89ae3424df4321ac
Dani <daniaxasabraxas@gmail.com>
2020-09-10 07:50:37 -0300

    Adding a Views and getting undefined urls

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(shopHomeRouter);
 app.use('/admin', adminRouter);
 
+// Get Error
+app.use((req, res, next) => {
+  return console.log('Url not exists');
+})
+
 app.listen(3000);
